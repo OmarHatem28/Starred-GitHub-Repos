12be23e3d588cc6f5c22ee1a0159b4664b9757c0
Omar <omarh.ismail1@gmail.com>
2018-10-23 11:56:14 +0200

    Added Network Access, Repos Presenter

--- app/src/main/java/com/example/warhit/starredrepos/Presenter/ReposPresenter.java
+++ app/src/main/java/com/example/warhit/starredrepos/Presenter/ReposPresenter.java
@@ -1,6 +1,8 @@
 package com.example.warhit.starredrepos.Presenter;
 
 import android.telecom.Call;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.warhit.starredrepos.Model.GitHubResponse;
 import com.example.warhit.starredrepos.Retrofit.GitHubClient;
@@ -10,6 +12,11 @@ import com.example.warhit.starredrepos.View.StarredReposView;
 import java.util.List;
 
 import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+import static android.content.ContentValues.TAG;
 
 public class ReposPresenter {
 
@@ -21,7 +28,28 @@ public class ReposPresenter {
     public void getStarredRepos(String userName){
         GitHubRequest gitHubRequest = GitHubClient.getInstance().create(GitHubRequest.class);
 
-        Observable<List<GitHubResponse>> call = gitHubRequest.getStarredRepos(userName);
+        Observable<List<GitHubResponse>> observable = gitHubRequest.getStarredRepos(userName);
+
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<GitHubResponse>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "In onCompleted()");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        Log.d(TAG, "In onError()");
+                    }
+
+                    @Override
+                    public void onNext(List<GitHubResponse> gitHubResponses) {
+                        Log.d(TAG, "In onNext()");
+//                        adapter.setGitHubRepos(gitHubRepos);
+                    }
+                });
     }
 
 }
