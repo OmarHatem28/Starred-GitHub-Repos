9b897f2c8d9a580e86941baffff75aad24e4801c
Omar <omarh.ismail1@gmail.com>
2018-10-23 15:00:31 +0200

    Fixed Some Layout Design Bugs

--- app/src/main/java/com/example/warhit/starredrepos/Activities/MainActivity.java
+++ app/src/main/java/com/example/warhit/starredrepos/Activities/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.warhit.starredrepos.Adapter.ReposAdapter;
 import com.example.warhit.starredrepos.Model.GitHubResponse;
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements StarredReposView{
 
     EditText userName;
     RecyclerView RV;
+    ProgressBar progressBar;
+    TextView empty;
 
     RecyclerView.LayoutManager layoutManager;
 
@@ -28,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements StarredReposView{
         setContentView(R.layout.activity_main);
 
         userName = findViewById(R.id.user);
+        empty = findViewById(R.id.empty);
+        progressBar = findViewById(R.id.indeterminateBar);
         RV=findViewById(R.id.RV);
         layoutManager=new LinearLayoutManager(this);
         RV.setLayoutManager(layoutManager);
@@ -35,17 +42,30 @@ public class MainActivity extends AppCompatActivity implements StarredReposView{
 
     public void getMyRepos(View view) {
 
+        if ( userName.getText().toString().isEmpty() ){
+            Toast.makeText(this,"Please Enter A Valid User Name",Toast.LENGTH_LONG).show();
+            return;
+        }
+
         ReposPresenter reposPresenter = new ReposPresenter(this);
         reposPresenter.getStarredRepos(userName.getText().toString());
+        progressBar.setVisibility(View.VISIBLE);
 
     }
 
     @Override
     public void showStarredRepos(List<GitHubResponse> StarredRepos) {
 
+        if ( StarredRepos.size() == 0 ){
+            empty.setText("This User Doesn't have Starred Repositories");
+        }
+        else{
+            empty.setText("");
+        }
         ReposAdapter reposAdapter = new ReposAdapter(StarredRepos);
         RV.setAdapter(reposAdapter);
         reposAdapter.notifyDataSetChanged();
+        progressBar.setVisibility(View.GONE);
 
     }
 }
